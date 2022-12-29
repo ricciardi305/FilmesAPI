438250344ed40a2c28b137669d8c675d46cb2869
Rafael Ricciardi <rafael.ricciardi@konia.com.br>
2022-12-29 17:52:27 -0300

    Dtos creation, update and delete routes and documentation with swagger

--- FilmesAPI/Data/Dtos/CreateFilmeDto.cs
+++ FilmesAPI/Data/Dtos/CreateFilmeDto.cs
@@ -1,23 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace FilmesAPI.Models;
+namespace FilmesAPI.Data.Dtos;
 
-public class Filme
+public class CreateFilmeDto
 {
-    [Key]
-    [Required]
-    public int Id { get; set; }
 
     [Required(ErrorMessage = "O título do filme é obrigatório!")]
     public string Titulo { get; set; }
 
     [Required(ErrorMessage = "O gênero do filme é obrigatório!")]
-    [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres!")]
+    [StringLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres!")]
     public string Genero { get; set; }
 
     public string Direcao { get; set; }
-    
+
     [Required]
     [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos!")]
     public int Duracao { get; set; }
+
 }
---
+++ FilmesAPI/Data/Dtos/ReadFilmeDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos;
+
+public class ReadFilmeDto
+{
+    public string Titulo { get; set; }
+
+    public string Genero { get; set; }
+
+    public string Direcao { get; set; }
+
+    public int Duracao { get; set; }
+    public DateTime HoraDaConsulta { get; set; } = DateTime.Now;
+}
