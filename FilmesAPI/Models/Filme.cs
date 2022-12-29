820ec71bbcee0eb48a1990a9540b97723e6f8b99
Rafael Ricciardi <rafael.ricciardi@konia.com.br>
2022-12-28 21:48:17 -0300

    retrieve filmes route

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -4,6 +4,8 @@ namespace FilmesAPI.Models;
 
 public class Filme
 {
+    public int Id { get; set; }
+
     [Required(ErrorMessage = "O título do filme é obrigatório!")]
     public string Titulo { get; set; }
 
@@ -11,11 +13,9 @@ public class Filme
     [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres!")]
     public string Genero { get; set; }
 
-    [Required(ErrorMessage = "Nome do Diretor(a) é obrigatório!")]
     public string Direcao { get; set; }
     
     [Required]
     [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos!")]
-    
     public int Duracao { get; set; }
 }
